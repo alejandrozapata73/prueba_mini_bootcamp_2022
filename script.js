376a1580f2f77ff4b4ed9bff028b80ce4f5ee680
alejandro <alejandrozapata73@gmail.com>
2022-04-21 19:39:24 -0300

    subiendo archivo script

---
+++ script.js
@@ -0,0 +1,70 @@
+// console.log('hola soy un condicional')
+
+// //operadores de relacion
+// /*
+
+// < menor... <=menor igual
+// > mayor--->= mayor igual
+// == igual
+// === igual estricto
+// != distinto
+// ! negado
+
+// */
+
+// var numeroA = 10
+
+// var numeroB = 25
+
+// console.log(10>5)// resultado boolean
+
+// console.log(!(numeroA>numeroB))
+
+// //condicionales...
+// /* si no se conectan...no aprenden...no sacan dudas...*/
+
+// //..true
+
+// var edadA = 15
+
+// const mayorEdad = 18
+
+// // if(edadA >mayorEdad) {
+// //     alert('Usted puede votar')
+// //     console.log('puede votar')
+// // }
+
+// var nombreUsuario = "angely"
+// const nombreUsuarioReal = "angely"
+// var vidas = 0
+
+
+// if(nombreUsuario===nombreUsuarioReal){
+   
+//     vidas = vidas + 1 //a un contador
+//     alert('usuario correcto...Bienvenido: ' + nombreUsuario + vidas)
+//     nombreUsuario = "Probando"
+//     vidas = vidas + 1
+//     alert('usuario correcto...Bienvenido: ' + nombreUsuario + vidas)
+//     var totalVidas = vidas //scope
+// }else{
+//     alert('Usuario incorrecto: ' + nombreUsuario + vidas)
+// }
+
+
+// var passIngreso = "1234"
+// const passReal = 1234
+
+// if(passIngreso===passReal){
+//     alert('contraseña correcta.. puede ingresar')
+// }else{
+//     alert('password incorrecto')
+// }
+
+// alert('El total de vidas es: ' + totalVidas)
+
+
+
+
+
+
